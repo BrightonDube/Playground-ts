a3dd09a8bce342cfd35f8c868f63534b763ade94
BrightonDube <BrightonDube@users.noreply.github.com>
2022-01-04 20:43:42 +0200

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+var hello = 'Hello World';
+console.log(a);
+var sayHello = function (name) {
+    return "Hello ".concat(name);
+};
+f();
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+let hello: string = 'Hello World'
+console.log(a)
+const sayHello = (
+    name: string
+): string /* It's always  better to specify the return type */ => {
+    return `Hello ${name}`
+}
+f()
