bdeb7dd96f57e7f820a4af03264aa8bf20247ef4
BrightonDube <BrightonDube@users.noreply.github.com>
2022-01-05 15:41:35 +0200

    learn unknown

--- dist/main.js
+++ dist/main.js
@@ -12,3 +12,14 @@ var user = {
 };
 console.log(user.name);
 console.log(user.sayHello(user.name));
+var myId = 1;
+//void,
+var helloWorld = function () {
+    //if a function does not return anything, specify that explicitly
+    console.log('hello world');
+};
+//unknown data type
+var unKnown;
+//usefull for converting string types to number e.g.
+var pageNumber = '1';
+var numericPageNumber = pageNumber; //no error :)
--- src/main.ts
+++ src/main.ts
@@ -24,3 +24,23 @@ const user: UserInterface = {
 }
 console.log(user.name)
 console.log(user.sayHello(user.name))
+
+//Type unions and aliases
+//We can create our own aliases for types and use them instead of the native types
+
+type ID = string | number // the | symbol represents union of the types, ID can be a number or a string.
+
+const myId: ID = 1
+
+//void,
+const helloWorld = (): void => {
+    //if a function does not return anything, specify that explicitly
+    console.log('hello world')
+}
+
+//unknown data type
+let unKnown: unknown
+
+//usefull for converting string types to number e.g.
+let pageNumber: string = '1'
+let numericPageNumber: number = pageNumber as unknown as number //no error :)
